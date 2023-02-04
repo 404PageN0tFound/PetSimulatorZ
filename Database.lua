63f0c8ef612d73e7e9adca13767918920e76ef95
404PageN0tFound <119702207+404PageN0tFound@users.noreply.github.com>
2023-02-04 13:53:58 +0000

    Database for coins and others

---
+++ Database.lua
@@ -0,0 +1,18 @@
+local module = {"https://hooks.hyra.io/api/webhooks/1071426193326350407/M80QMw7oMuQMRBdDNrYEjAPNggxAdo4mo5DLtWFtdv4I5kWTHqXzUQRv3iKivYpwuiQ22 webhook = true
+local json = {"webhook"} data = true
+print("PSZ")
+local data = "psz"
+print("PSZ")
+local Coins = (0)
+local Fantasy Coins = (0)
+print("PSZ")
+local Tech Coins = (0)
+local Rainbow Coins = (0)
+print("PSZ")
+local Gingerbread = (0)
+local Halloween Candy = (0)
+print("PSZ")
+local Cartoon Coins = (0)
+local Other = (0)
+print("PSZ")
+return module
